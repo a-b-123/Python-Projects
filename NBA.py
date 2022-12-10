7ab62def3c3ac0258942b1042b1d424855ba9348
Aditya Bidnur <41347991+a-b-123@users.noreply.github.com>
2022-12-10 16:11:16 -0600

    Add files via upload

---
+++ NBA.py
@@ -0,0 +1,41 @@
+from requests import get
+from pprint import PrettyPrinter
+
+mainurl = "https://nba-prod-us-east-1-mediaops-stats.s3.amazonaws.com"
+json = "/NBA/liveData/scoreboard/todaysScoreboard_00.json"
+
+printer = PrettyPrinter()
+
+response = get(mainurl + json).json()
+sboard = response['scoreboard']
+printer.pprint(sboard.keys())
+
+def info_games():
+    games = get(mainurl + json).json()['scoreboard']['games']
+    
+    for game in games:
+        home = game['homeTeam']
+        away = game['awayTeam']
+        quarter = game['period']
+        clock = game["gameClock"]
+        Hscore = game['homeTeam']['score']
+        Ascore = game['awayTeam']['score']
+        topPlayerHome = game['gameLeaders']['homeLeaders']
+        topPlayerAway = game['gameLeaders']['awayLeaders']
+        print(f"--------------")
+
+
+        #-----score and game info
+        print(f"{home['teamTricode']} vs {away['teamTricode']}")
+        print(f"{Hscore} to {Ascore}")
+        print(f"{clock}" ,"|", "Q:", f"{quarter}")
+
+
+        #------top players P/R/A
+        print("*","Game Leaders:")
+        print(f"{topPlayerHome['name']}:","Points:",f"{topPlayerHome['points']}"",",
+        "Rebounds:",f"{topPlayerHome['rebounds']}"",","Assists:",f"{topPlayerHome['assists']}"  )
+        print(f"{topPlayerAway['name']}:","Points:",f"{topPlayerAway['points']}"",",
+        "Rebounds:",f"{topPlayerAway['rebounds']}"",","Assists:",f"{topPlayerAway['assists']}"  )
+        
+info_games()
